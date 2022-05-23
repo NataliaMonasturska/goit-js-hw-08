5b3b4288e4a1d055beec26a54728d98fbf4c2ba9
Natalia29011989 <tjomamon@gmail.com>
2022-05-23 22:47:27 +0300

    3 незаконченое кроме 3 пункта

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import Thrrottle from "lodash.throttle";
+const a = {};
+saveForm();
+const form = document.querySelector('form');
+console.log(form);
+form.addEventListener('input', Thrrottle(setLocalStorageValue,500) )
 
+function setLocalStorageValue(e) {
+      a[e.target.name] = e.target.value;
+    
+    console.log(e.target.name);
+    // let x = e.target.value;
+    
+   localStorage.setItem("feedback-form-state", JSON.stringify(a));  
+}
+
+
+form.addEventListener("submit", onFormSubmit)
+function onFormSubmit(e) {
+    e.preventDefault();  
+    e.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state")
+}
+
+
+function saveForm() {
+    const saveMesssage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    // const s = saveMesssage[e.target.name]; 
+    if (saveMesssage) {
+        a.textarea.value = s;
+    }
+}
