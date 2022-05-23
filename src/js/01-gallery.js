70869b54898baf551dc56fc9d46e45f72ea860af
Natalia29011989 <tjomamon@gmail.com>
2022-05-23 12:11:40 +0300

    1задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightboxEnd from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItemsEnd } from './gallery-items.js';
+
+
+
+
+const ulGallery = document.querySelector('.gallery')
+const galleryItemsNew = galleryItems.map((item) => 
+    
+ `<li><a class="gallery__item"
+  href=${item.original}>
+  <img 
+  class="gallery__image" 
+  src=${item.preview}
+  alt=${item.description}
+  title=${item.description} />
+</a></li>`  
+).join("");
+    
+ulGallery.insertAdjacentHTML("afterbegin", galleryItemsNew);
+
+let lightbox = new SimpleLightbox('.gallery a');
+
+lightbox.on('show.simplelightbox', function () {
+    lightbox.defaultOptions.captionDelay = 250;
+	
+});
+
+
+
+
+
